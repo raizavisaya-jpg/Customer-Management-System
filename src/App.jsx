d56a87d30492a879a29dc2feceac34bd8a7a01d7
Tristan Alejandro <tristanjayreyes.alejandro06@gmail.com>
2026-05-16 01:24:59 +0800

    Resolved App.jsx merge conflict from dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage.jsx";
@@ -119,7 +119,7 @@ function App() {
 }
 
 export default App;
-=======
+
 import React from 'react';
 import Login from './Login';
 import { useAuth } from './contexts/AuthContext';
@@ -163,4 +163,4 @@ function App() {
 }
 
 export default App;
->>>>>>> origin/dev
+
